4dc11d27ffe11a80b22972d7eb1642cf78da69b6
Peter Eichman <peichman@umd.edu>
2017-03-02 16:21:53 -0500

    write annotation lists to files

--- getmets.rb
+++ getmets.rb
@@ -6,6 +6,8 @@ require 'json'
 require 'nokogiri'
 require './alto.rb'
 require 'openssl'
+require 'erb'
+require 'fileutils'
 
 class ImageStub
   def initialize(x_resolution, y_resolution)
@@ -20,21 +22,30 @@ class ImageStub
   end
 end
 
+@fcrepo_base_url = 'https://fcrepolocal/fcrepo/rest/'
+@fcrepo_ssl_verify = false
+@iiif_base_url = 'https://iiiflocal/'
+@solr_base_url = 'https://solrlocal:8984/'
+@solr_ssl_verify = false
+@target_dir = 'annotations'
+issue_uri = 'https://fcrepolocal/fcrepo/rest/pcdm/60/b8/e9/d8/60b8e9d8-ee06-4069-b7b2-5c7a96cd88a7'
+
+def iiif_id(uri)
+  'fcrepo:' + ERB::Util.url_encode(uri[@fcrepo_base_url.length..uri.length])
+end
+
 nsmap = {
   'mets'  => 'http://www.loc.gov/METS/',
   'xlink' => 'http://www.w3.org/1999/xlink',
   'mix'   => 'http://www.loc.gov/mix/',
 }
 
-property = { solr_host: 'solrlocal:8984' }
-
-solr_conn = Faraday.new(:url => "https://#{property[:solr_host]}", ssl: { verify: false }) do |faraday|
+solr_conn = Faraday.new(url: @solr_base_url, ssl: { verify: @solr_ssl_verify }) do |faraday|
   # parse all responses as JSON, since Solr returns JSON as "text/plain"
   faraday.response :json
   faraday.adapter  Faraday.default_adapter
 end
 
-issue_uri = 'https://fcrepolocal/fcrepo/rest/pcdm/55/e2/be/6e/55e2be6e-68d5-4f61-8781-f6fa8931bbd7'
 solr_response = solr_conn.get '/solr/fedora4/select',
   'q' => "id:#{issue_uri.gsub(':', '\:')}",
   'fl' => '*,mets:[subquery],articles:[subquery],p:[subquery]',
@@ -51,25 +62,24 @@ solr_response = solr_conn.get '/solr/fedora4/select',
   'p.ocr.q' => '{!terms f=id v=$row.pcdm_files}',
   'p.ocr.fq' => 'rdf_type:pcdmuse\:ExtractedText',
   'p.ocr.fl' => 'id,title',
-  'p.master.q' => '{!terms f=id v=$row.pcdm_files}',
-  'p.master.fq' => 'rdf_type:pcdmuse\:PreservationMasterFile',
-  'p.master.fl' => 'id,title,image_height,image_width',
   'wt' => 'json'
 document = solr_response.body['response']['docs'][0]
-#puts JSON.dump(document)
-#exit
 
-conn = Faraday.new(ssl: { verify: false })
+conn = fcrepo_update_conn = Faraday.new(
+  ssl: {
+    verify: false,
+    client_cert: OpenSSL::X509::Certificate.new(File.open('/Users/peichman/batchload/fcrepolocal/batchloader.pem', 'r')),
+    client_key: OpenSSL::PKey::RSA.new(File.open('/Users/peichman/batchload/fcrepolocal/batchloader.key', 'r')),
+  }
+)
 
-article_xml_uri = document['mets']['docs'].select{|doc| doc['title'][0] =~ /article METS metadata/ }[0]['pcdm_files'][0]
-issue_xml_uri = document['mets']['docs'].select{|doc| doc['title'][0] =~ /issue METS metadata/ }[0]['pcdm_files'][0]
+article_xml_uri = document['mets']['docs'].select{|doc| doc['title'][0] =~ /article/ }[0]['pcdm_files'][0]
+issue_xml_uri = document['mets']['docs'].select{|doc| doc['title'][0] =~ /issue/ }[0]['pcdm_files'][0]
 
 issue_xmldoc = Nokogiri::XML(conn.get(issue_xml_uri).body)
 issue_mets = issue_xmldoc.xpath('/mets:mets', nsmap).first
 
 article_resources = document['articles']['docs'].clone
-#puts JSON.dump(article_resources)
-#exit
 
 page_resources = {}
 pages = document['p']['docs']
@@ -86,24 +96,8 @@ pages.map do |page|
     id: page['id'],
     ocrfile: ocr_uri,
     ocr: ocr_resource,
-    masterfile: page['master'][0]['id'],
-    image_width: page['master'][0]['image_width'],
-    image_height: page['master'][0]['image_height'],
   }
 end
-
-#puts JSON.dump(page_resources)
-#exit
-#puts article_xml_uri
-#puts issue_xml_uri
-
-fcrepo_update_conn = Faraday.new(
-  ssl: {
-    verify: false,
-    client_cert: OpenSSL::X509::Certificate.new(File.open('/Users/peichman/batchload/fcrepolocal/batchloader.pem', 'r')),
-    client_key: OpenSSL::PKey::RSA.new(File.open('/Users/peichman/batchload/fcrepolocal/batchloader.key', 'r')),
-  }
-)
 
 article_annotations = []
 res = conn.get(article_xml_uri)
@@ -129,8 +123,10 @@ article_mets.xpath('mets:structMap//mets:div[@TYPE="article"]', nsmap).each do |
       article_tagged_text += block.tagged_text(ocr)
       article_pages.push(page[:pagenum])
 
+      canvas_uri = "#{@iiif_base_url}manifests/#{iiif_id(issue_uri)}/canvas/#{iiif_id(page[:id])}"
+
       annotations.push({
-        "@id" => "#{fileid}-#{block.id}",
+        "@id" => blockid,
         "@type" => ["oa:Annotation", "umd:Article"],
         "resource" => [
           {
@@ -145,51 +141,42 @@ article_mets.xpath('mets:structMap//mets:div[@TYPE="article"]', nsmap).each do |
             "@type" => "oa:FragmentSelector",
             "value" => "xywh=#{ocr.coordinates(block)}",
           },
-          "full" => page[:id],
+          "full" => canvas_uri,
         },
-        "motivation" => "oa:highlighting"
+        "motivation" => "sc:painting"
       })
     end
-    article_annotations.push({
-      label: article_title,
-      annotations: annotations,
-    })
+    annotation_list = {
+      "@context" => "http://iiif.io/api/presentation/2/context.json",
+      '@id' => "#{@iiif_base_url}manifests/#{iiif_id(issue_uri)}/list/#{iiif_id(article_uri)}",
+      'label' => article_title,
+      'resources' => annotations,
+    }
 
     article_pages.uniq!.sort!
-    #puts article_title
-    #puts article_tagged_text
-    #puts
-    solr_response = solr_conn.get
 
-    solr_response = solr_conn.post do |req|
-      req.url '/solr/fedora4/update'
-      req.headers['Content-Type'] = 'application/json'
-      req.body = JSON.dump({
-        'add' => {
-          'doc' => {
-            'id' => article_uri,
-            'ocr_text' => { 'set' => article_text },
-            'ocr_text_tagged' => { 'set' => article_tagged_text },
-          },
-        },
-        'commit' => {},
-      })
-
-      sparql_update = <<END
+    sparql_update = <<END
 PREFIX bibo: <http://purl.org/ontology/bibo/>
 
 INSERT DATA {
-  <> bibo:content """#{article_text}""" ;
-    bibo:pageStart #{article_pages.first} ;
+  <> bibo:pageStart #{article_pages.first} ;
     bibo:pageEnd #{article_pages.last} .
 }
 END
-      fcrepo_res = fcrepo_update_conn.patch do |req|
-        req.url article_uri
-        req.headers['Content-Type'] = 'application/sparql-update'
-        req.body = sparql_update
-      end
+    fcrepo_res = fcrepo_update_conn.patch do |req|
+      req.url article_uri
+      req.headers['Content-Type'] = 'application/sparql-update'
+      req.body = sparql_update
     end
+
+
+    FileUtils.mkpath "#{@target_dir}/#{iiif_id(issue_uri)}/list/#{iiif_id(article_uri)}"
+    File.open("#{@target_dir}/#{iiif_id(issue_uri)}/list/#{iiif_id(article_uri)}/list.json", 'w') do |file|
+      file.write JSON.dump(annotation_list)
+    end
+
+    article_annotations.push annotation_list
+
   else
     #puts "No article URI with title matching #{article_title}"
   end
